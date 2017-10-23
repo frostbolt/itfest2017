947ec252d9a2ba4c8d8a1b187fabab76c270645e
Sergey <me@serge.xyz>
2017-10-23 19:18:09 +0300

    minor changes.

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('js', function() {
 
 gulp.task('sass', function() {
 	return gulp.src('app/sass/**/*.scss')
-	.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+	.pipe(sass({outputStyle: 'compressed'}).on("error", notify.onError()))
 	.pipe(rename({suffix: '.min', prefix : ''}))
 	.pipe(autoprefixer(['last 15 versions']))
 	// .pipe(cleanCSS()) // Опционально, закомментировать при отладке
